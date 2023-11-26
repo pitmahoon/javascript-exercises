944b4c87eff257de69319f996590f95bcf148d7b
PitMahoon <59979243+pitmahoon@users.noreply.github.com>
2023-11-26 22:44:19 +0100

    Solution implemented

--- 03_reverseString/README.md
+++ 03_reverseString/README.md
@@ -1,4 +1,4 @@
-# Exercise 03 - Reverse a String
+# Exercise 03 - Reverse a String <- DONE
 
 Pretty simple, write a function called `reverseString` that returns its input, reversed!
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let i = inputString.length;
+    let reversedString = "";
+    for (;i >= 0; i--) {
+        reversedString += inputString.substr(i, 1);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
