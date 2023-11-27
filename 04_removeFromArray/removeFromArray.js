3845c0c4823b867eb6959c5b6f4fa0d359b0fea0
PitMahoon <59979243+pitmahoon@users.noreply.github.com>
2023-11-27 21:01:34 +0100

    Solution implemented

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...remChar) {
+    let i = 0;
+    let outputArray = inputArray;
+    // Selecting Character to be removed
+    do {
+        outputArray = outputArray.filter((char) => char !== remChar[i]);
+        i++;
+    } while (i<remChar.length);
+    return outputArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
