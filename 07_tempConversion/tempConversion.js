8e5c8679de7a217c1cbdb9ce456f8d88f446a914
PitMahoon <59979243+pitmahoon@users.noreply.github.com>
2023-11-28 12:39:08 +0100

    Rounding for toCelsius added and verified by Test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,5 +1,7 @@
 const convertToCelsius = function(temperature) {
-  return ((temperature - 32) * 5/9);
+  let tempCelsius = (temperature - 32) * 5/9;
+  tempCelsius = Math.round(tempCelsius * 10) / 10;
+  return tempCelsius;
 };
 
 const convertToFahrenheit = function(temperature) {
